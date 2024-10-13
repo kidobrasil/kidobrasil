cb35cbae28651afaea54da45c3d65e51a8461e95
vinex <notrealmailfornow@proton.me>
2024-10-13 17:13:57 -0300

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Add click or scroll-triggered interaction if needed
+document.querySelectorAll('.milestone').forEach(milestone => {
+    milestone.addEventListener('click', () => {
+        const tooltip = milestone.querySelector('.milestone-info');
+        tooltip.classList.toggle('hidden');
+    });
+});
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('hidden'); // Toggle visibility of mobile menu
+});
+
+
+
+
+
